2e3482a89f52eb03747366e26afd5c1db307cb40
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-07-20 15:11:03 +0300

    fixed a logical error in check()

--- src/AppBundle/Service/AnswerChecker.php
+++ src/AppBundle/Service/AnswerChecker.php
@@ -18,19 +18,25 @@ class AnswerChecker
     public function check(array $qas, array $answers)
     {
         $percentages = [];
+        $correct = [];
 
         foreach ($qas as $id => $qa) {
             if (!$answers[$id]) {
                 $percentages[$id] = 0;
                 continue;
             }
+            $percentages[$id] = 0;
+            $correct[$id] = 0;
             /** @var Answer $answer */
             foreach ($qa->answers as $answer) {
                 if ($answer->correct && isset($answers[$id][$answer->id])) {
                     $percentages[$id]++;
+                    $correct[$id]++;
+                } elseif ($answer->correct) {
+                    $correct[$id]++;
                 }
             }
-            $percentages[$id] = $percentages[$id] / count($qa->answers) * 100;
+            $percentages[$id] = $percentages[$id] / $correct[$id] * 100;
         }
 
         $percentages['final'] = array_sum($percentages) / count($percentages);
